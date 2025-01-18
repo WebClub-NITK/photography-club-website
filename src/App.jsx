0a16059f8ae3c18a7c0f5d484d8f80d3efa81eb9
Sahil kumar <supersen14@gmail.com>
2025-01-19 01:28:10 +0530

    individual portfolio page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { Route, Routes } from "react-router"
-import HomePage from "./pages/home"
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/home";
 
-import PhotoReels from './pages/photoreel'
-import Header from "./components/header"
+import PhotoReels from "./pages/photoreel";
+import Header from "./components/header";
 
-import Footer from "./components/footer"
-import PortfolioPage from "./pages/portfolio"
-import { ThemeProvider } from "./context/ThemeContext"
+import Footer from "./components/footer";
+import PortfolioPage from "./pages/portfolio";
+import IndividualPortfoli from "./pages/portfolio/individualPortfolio";
+import { ThemeProvider } from "./context/ThemeContext";
 
 export default function App() {
-	return (
-		<div>
-			<ThemeProvider>
-				<Header />
-				<div className="pt-[65px]">
-					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/portfolio" element={<PortfolioPage />} />
-            <Route path="/photo-reels" element={<PhotoReels/>}/>
-					</Routes>
-				</div>
-				<Footer />
-			</ThemeProvider>
-		</div>
-	)
+  return (
+    <div>
+      <ThemeProvider>
+        <Header />
+        <div className="pt-[65px]">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/portfolio" element={<PortfolioPage />} />
+			<Route path="/portfolio/:id" element={<IndividualPortfoli />} />
+            <Route path="/photo-reels" element={<PhotoReels />} />
+          </Routes>
+        </div>
+        <Footer />
+      </ThemeProvider>
+    </div>
+  );
 }
