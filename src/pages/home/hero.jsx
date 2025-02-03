62ce3ed55824b471c2b36d0cbf90c8c1b0615ccc
kumarutkarshuk <kumarutkarshuk18@gmail.com>
2025-02-03 09:28:30 +0530

    home: scroll behavior made smooth

--- src/pages/home/hero.jsx
+++ src/pages/home/hero.jsx
@@ -14,7 +14,11 @@ function Hero({ photos }) {
 
     const handlePhotoReelsClick = () => {
         navigate('/photo-reels');
-        window.scrollTo(0, 0);
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
     };
 
     return (
